1437a7114cde7db73234647b8fc499dc176e61d6
splashkida <seira.kida0406@gmail.com>
2021-02-23 17:25:09 +0900

    ransackの実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,23 @@
 class HomeController < ApplicationController
-  
+  before_action :search_memory, only: [:index, :search]
+
   def index
     @memory = Memory.order("created_at DESC")
+    set_memory_column 
   end
+
+  def search
+    @results = @p.result  # 検索条件にマッチした商品の情報を取得
+  end
+
+  private
+
+  def search_memory
+    @p = Memory.ransack(params[:q])  # 検索オブジェクトを生成
+  end
+
+  def set_memory_column
+    @memory_place = Memory.select("place").distinct  # 重複なくnameカラムのデータを取り出す
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'memories/index'
   get 'memories/new'
+  get 'memories/search'
+
   devise_for :users
   root to: 'home#index'
 
